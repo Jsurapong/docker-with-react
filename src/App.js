8d9a5ddb85dd20412f4faca2becb7b2cb7652765
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-09-08 15:54:21 +0700

    fixed docker-compose.yml

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Hello 2
+          Learn React Hello 222
         </a>
       </header>
     </div>
